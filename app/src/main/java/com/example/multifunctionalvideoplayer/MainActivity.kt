14e6f3bfd68661a5cfc67703efc15f9aac54f6c9
Amin Azizzade <amin.azizcomp24@gmail.com>
2024-05-04 15:12:59 +0400

    Link Finder Page main UI created

--- app/src/main/java/com/example/multifunctionalvideoplayer/MainActivity.kt
+++ app/src/main/java/com/example/multifunctionalvideoplayer/MainActivity.kt
@@ -4,12 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.multifunctionalvideoplayer.ui.theme.MultiFunctionalVideoPlayerTheme
 
@@ -19,29 +30,53 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MultiFunctionalVideoPlayerTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                VideoFinderFromLink()
             }
         }
     }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     MultiFunctionalVideoPlayerTheme {
-        Greeting("Android")
+        VideoFinderFromLink()
+    }
+}
+
+@Composable
+fun VideoFinderFromLink(
+) {
+    var text by remember { mutableStateOf("") }
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ) {
+        Row (
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        )
+        {
+
+            OutlinedTextField(
+                value = text,
+                onValueChange = { text = it },
+                label =
+                {
+                    Text("Label")
+                }
+            )
+
+            IconButton(
+                onClick =
+                {
+
+                }
+            )
+            {
+                Icon(painter = painterResource(id = R.drawable.ic_launcher_foreground), contentDescription = null)
+            }
+        }
+
     }
 }
