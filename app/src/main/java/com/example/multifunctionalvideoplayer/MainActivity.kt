df4ff571aebf200067a774148de3b3d87f5383e4
Amin Azizzade <amin.azizcomp24@gmail.com>
2024-05-07 11:34:50 +0400

    Coding UI interface

--- app/src/main/java/com/example/multifunctionalvideoplayer/MainActivity.kt
+++ app/src/main/java/com/example/multifunctionalvideoplayer/MainActivity.kt
@@ -60,6 +60,7 @@ class MainActivity : ComponentActivity() {
         Surface(
             modifier = Modifier.fillMaxSize(),
             color = MaterialTheme.colorScheme.background
+
         ) {
             Row(
                 horizontalArrangement = Arrangement.Center,
